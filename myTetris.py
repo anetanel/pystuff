93d6540a70508f9eaa0f5c9da5e598d7996844fc
anetanel <netanel.attali@gmail.com>
2022-08-20 17:05:35 +0300

    tweak menus and setting

--- myTetris.py
+++ myTetris.py
@@ -3,10 +3,11 @@ from enum import Enum, auto
 
 import pygame
 from pygame import mixer
+import pygame_menu
 
 
 class Colors(Enum):
-    BLACK = (0, 0, 0)
+    BLACK = (1, 1, 1)
     WHITE = (255, 255, 255)
     GRAY = (128, 128, 128)
     COLOR_1 = (120, 37, 179)
@@ -16,6 +17,7 @@ class Colors(Enum):
     COLOR_5 = (180, 34, 22)
     COLOR_6 = (180, 34, 122)
     COLOR_7 = (255, 125, 0)
+    RED = (255, 0, 0)
 
 
 # noinspection PyArgumentList
@@ -66,11 +68,11 @@ class Figure:
 
 
 class Tetris:
-    block_size = 20
     x = 0
     y = 0
 
     def __init__(self, width, height):
+        self.block_size = SCREEN_WIDTH * 0.02
         self.score = 0
         self.state = GameState.RUNNING
         self.height = height
@@ -106,7 +108,7 @@ class Tetris:
                 self.go_side(1)
 
     def go_side(self, dx):
-        sfx_move.play()
+        sfx['move'].play()
         old_x = self.figure.x
         self.figure.x += dx
         if self.intersects():
@@ -119,7 +121,7 @@ class Tetris:
             self.freeze()
 
     def rotate(self):
-        sfx_rotate.play()
+        sfx['rotate'].play()
         old_rotation = self.figure.rotation
         self.figure.rotate()
         if self.intersects():
@@ -144,7 +146,7 @@ class Tetris:
         return intersection
 
     def freeze(self):
-        sfx_drop.play()
+        sfx['drop'].play()
         for i in range(4):
             for j in range(4):
                 if i * 4 + j in self.figure.image():
@@ -158,7 +160,7 @@ class Tetris:
         lines = 0
         for i in range(1, self.height):
             if Colors.WHITE not in self.field[i]:
-                sfx_break_line.play()
+                sfx['break_line'].play()
                 lines += 1
                 for i1 in range(i, 1, -1):
                     self.field[i1] = self.field[i1 - 1].copy()
@@ -172,8 +174,21 @@ def new_game():
 
 
 def main():
+    def set_music_volume(value):
+        pygame.mixer.music.set_volume(value / 10)
+
+    def set_sfx_volume(value):
+        for s in sfx.values():
+            s.set_volume(value / 10)
+        sfx['game_over'].play()
+
+    def toggle_grid(_=None):
+        global draw_grid
+        draw_grid = not draw_grid
+        settings_menu.get_widget(widget_id='grid').set_value(draw_grid)
+
     # Initialize game
-    screen = pygame.display.set_mode(size=(SCREEN_WIDTH, SCREEN_HEIGHT), flags=pygame.SCALED)
+    screen = pygame.display.set_mode(size=(SCREEN_WIDTH, SCREEN_HEIGHT))
     pygame.display.set_caption("Tetris")
     game = new_game()
     clock = pygame.time.Clock()
@@ -181,12 +196,72 @@ def main():
     pressing_down = False
     fps = 25
     counter = 0
+    draw_grid = False
+
+    main_menu = pygame_menu.Menu(
+        center_content=True,
+        height=SCREEN_HEIGHT,
+        theme=theme,
+        title='Tetris - Main Menu',
+        width=SCREEN_WIDTH
+    )
+
+    about_menu = pygame_menu.Menu(
+        center_content=True,
+        height=main_menu.get_height(),
+        mouse_visible=False,
+        title='About',
+        width=main_menu.get_width(),
+        theme=main_menu.get_theme()
+    )
+    for m in ABOUT:
+        about_menu.add.label(m, margin=(0, 0))
+    about_menu.add.label('')
+    about_menu.add.button('Return to Menu', pygame_menu.events.BACK)
+
+    help_menu = pygame_menu.Menu(
+        center_content=True,
+        height=main_menu.get_height(),
+        mouse_visible=False,
+        title='Help',
+        width=main_menu.get_width(),
+        theme=main_menu.get_theme()
+    )
+    help_menu.add.label('Keys', underline=True, underline_width=10).set_font_shadow(enabled=True)
+    for m in HELP:
+        help_menu.add.label(m, align=pygame_menu.locals.ALIGN_LEFT)
+    help_menu.add.label('')
+    help_menu.add.button('Return to Menu', pygame_menu.events.BACK)
+
+    settings_menu = pygame_menu.Menu(
+        center_content=True,
+        height=main_menu.get_height(),
+        mouse_visible=False,
+        title='Settings',
+        width=main_menu.get_width(),
+        theme=main_menu.get_theme()
+    )
+    settings_menu.add.range_slider(title='Music Volume: ', default=10, range_values=[*range(0, 11)],
+                                   onchange=set_music_volume, range_text_value_enabled=False)
+    settings_menu.add.range_slider(title='SFX Volume: ', default=10, range_values=[*range(0, 11)],
+                                   onchange=set_sfx_volume, range_text_value_enabled=False)
+    settings_menu.add.toggle_switch("Enable Grid", default=draw_grid, onchange=toggle_grid, toggleswitch_id='grid')
+    settings_menu.add.label('')
+    settings_menu.add.button('Return to Menu', pygame_menu.events.BACK)
+
+    main_menu.add.button('Play', main_menu.disable)
+    main_menu.add.button(settings_menu.get_title(), settings_menu)
+    main_menu.add.button(help_menu.get_title(), help_menu)
+    main_menu.add.button(about_menu.get_title(), about_menu)
+    main_menu.add.button('Quit', pygame_menu.events.EXIT)
+    main_menu.mainloop(screen)
 
     # Text
-    small_font = pygame.font.SysFont('Calibri', 25, True, False)
-    large_font = pygame.font.SysFont('Calibri', 65, True, False)
-    text_game_over = large_font.render("Game Over", True, Colors.COLOR_7.value)
-    text_pause = large_font.render("PAUSE", True, Colors.GRAY.value)
+    small_font = pygame.font.SysFont('Calibri', int(SCREEN_WIDTH * 0.02), True, False)
+    large_font = pygame.font.SysFont('Calibri', int(SCREEN_WIDTH * 0.1), True, False)
+    text_game_over1 = large_font.render("Game Over", True, Colors.WHITE.value, Colors.BLACK.value)
+    text_game_over2 = small_font.render("Press 'r' to Restart...", True, Colors.WHITE.value, Colors.BLACK.value)
+    text_pause = large_font.render("PAUSE", True, Colors.BLACK.value, Colors.GRAY.value)
 
     # Main game loop
     while not done:
@@ -199,7 +274,8 @@ def main():
         if counter % (fps // game.level) == 0 or pressing_down:
             game.move(Direction.DOWN)
 
-        for event in pygame.event.get():
+        events = pygame.event.get()
+        for event in events:
             if event.type == pygame.QUIT:
                 done = True
             elif event.type == pygame.KEYDOWN:
@@ -226,6 +302,11 @@ def main():
                     elif game.state == GameState.PAUSE:
                         game.state = GameState.RUNNING
                         mixer.music.unpause()
+                elif event.key == pygame.K_F1:
+                    main_menu.enable()
+                    main_menu.mainloop(screen)
+                elif event.key == pygame.K_g:
+                    toggle_grid()
 
             elif event.type == pygame.KEYUP:
                 if event.key == pygame.K_DOWN:
@@ -241,9 +322,10 @@ def main():
         for y in range(game.height):
             for x in range(game.width):
                 # Draw grid
-                # pygame.draw.rect(screen, Colors.GRAY.value,
-                #                  [game.x + game.block_size * x, game.y + (game.block_size * y),
-                #                   game.block_size, game.block_size], 1)
+                if draw_grid:
+                    pygame.draw.rect(screen, Colors.GRAY.value,
+                                     [game.x + game.block_size * x, game.y + (game.block_size * y),
+                                      game.block_size, game.block_size], 1)
                 # Draw frozen figures
                 if game.field[y][x] != Colors.WHITE.name:
                     pygame.draw.rect(screen, game.field[y][x].value,
@@ -260,6 +342,11 @@ def main():
                                          [game.x + game.block_size * (j + game.figure.x) + 1,
                                           game.y + game.block_size * (i + game.figure.y) + 1,
                                           game.block_size - 1, game.block_size - 1])
+                        pygame.draw.rect(screen, Colors.RED.value,
+                                         [game.x + game.block_size * (j + game.figure.x) + 1,
+                                          game.y + game.block_size * (i + game.figure.y) + 50,
+                                          game.block_size - 1, game.block_size - 1], 1)
+
                     # Draw next figure preview
                     if p in game.next_figure.image():
                         pygame.draw.rect(screen, game.next_figure.color.value,
@@ -272,31 +359,65 @@ def main():
         if game.state == GameState.GAME_OVER:
             if not game.game_over:
                 mixer.music.stop()
-                sfx_game_over.play()
+                sfx['game_over'].play()
                 game.game_over = True
-            screen.blit(text_game_over, text_game_over.get_rect(center=(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)))
+            screen.blit(text_game_over1, text_game_over1.get_rect(center=(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)))
+            screen.blit(text_game_over2, text_game_over2.get_rect(
+                center=(SCREEN_WIDTH / 2,
+                        SCREEN_HEIGHT / 2 + text_game_over1.get_size()[1] / 2 + text_game_over2.get_size()[1] / 2)
+            ))
         if game.state == GameState.PAUSE:
             screen.blit(text_pause, text_pause.get_rect(center=(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)))
         text_score = small_font.render("Score: " + str(game.score), True, Colors.BLACK.value)
         text_level = small_font.render("Level: " + str(game.level), True, Colors.BLACK.value)
+        text_help = small_font.render("F1: Menu", True, Colors.GRAY.value)
         screen.blit(text_score, [0, 0])
-        screen.blit(text_level, [0, 30])
+        screen.blit(text_level, [0, text_score.get_size()[1] * 1.5])
+        screen.blit(text_help, [0, SCREEN_HEIGHT - text_help.get_size()[1]])
 
-        # screen.blit(debug, [0,0])
         pygame.display.flip()
         clock.tick(fps)
 
     pygame.quit()
 
 
+__version__ = '0.1'
+__author__ = 'Netanel Attali'
+__email__ = 'netanel.attali@gmail.com'
+__title__ = 'myTetris'
+
+pygame.init()
+mixer.music.load('sound/tetris-music.mp3')
+sfx = {
+    'move': mixer.Sound('sound/move.mp3'),
+    'rotate': mixer.Sound('sound/rotate.mp3'),
+    'drop': mixer.Sound('sound/drop.mp3'),
+    'game_over': mixer.Sound('sound/game_over.mp3'),
+    'break_line': mixer.Sound('sound/break_line.mp3')
+}
+
+SCREEN_WIDTH = 3840
+SCREEN_HEIGHT = 2160
+
+ABOUT = [f'{__title__} {__version__}',
+         f'Author: {__author__}',
+         '',
+         f'Email: {__email__}']
+
+HELP = ['F1 : Toggle Main Menu',
+        'ESC : Quit Game',
+        'R : Restart Game',
+        'P : Pause Game',
+        'G : Toggle Grid',
+        'LEFT/RIGHT/DOWN : Move Piece',
+        'UP : Rotate Piece',
+        'SPACE : Drop Piece'
+        ]
+theme = pygame_menu.themes.THEME_DEFAULT.copy()
+theme.widget_font_size = int(SCREEN_HEIGHT * 0.05)
+theme.title_font_size = int(SCREEN_HEIGHT * 0.05)
+draw_grid = False
+show_ghost = True
+
 if __name__ == '__main__':
-    pygame.init()
-    mixer.music.load('sound/tetris-music.mp3')
-    sfx_move = mixer.Sound('sound/move.mp3')
-    sfx_rotate = mixer.Sound('sound/rotate.mp3')
-    sfx_drop = mixer.Sound('sound/drop.mp3')
-    sfx_game_over = mixer.Sound('sound/game_over.mp3')
-    sfx_break_line = mixer.Sound('sound/break_line.mp3')
-    SCREEN_WIDTH = 800
-    SCREEN_HEIGHT = 600
     main()
